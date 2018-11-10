483111f4dfb34f5ff504be9c8f50944f175e0ed8
Rodell Rodriguez <rodriguez.rodell53@gmail.com>
2018-11-09 22:35:10 -0500

    Removed env folder from repo

--- backend/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\rodell\desktop\coding\github\fantasy-goat\backend\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
